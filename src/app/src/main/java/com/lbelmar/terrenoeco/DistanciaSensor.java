b679ae48f5d48c5bebbc635a1700e4454a7df940
Adrian <admallla@epsg.upv.es>
2021-11-09 11:53:25 +0100

    Uso de strings

--- src/app/src/main/java/com/lbelmar/terrenoeco/DistanciaSensor.java
+++ src/app/src/main/java/com/lbelmar/terrenoeco/DistanciaSensor.java
@@ -1,21 +1,32 @@
 package com.lbelmar.terrenoeco;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 public class DistanciaSensor {
 
     float umbralMuyAlto =  -80;
     float umbralAlto =  -75;
     float umbralCerca =  -40;
 
+
+    private Resources resources;
+
+
+    public DistanciaSensor(){
+        resources = MainActivity.getContext().getResources();
+    }
+
     public String distanciaRelativa(float valor){
 
         if(valor < umbralMuyAlto){
-            return "Muy lejos";
+            return resources.getString(R.string.textoSensorMuyLejos);
         }else if(valor < umbralAlto){
-            return "Lejos";
+            return resources.getString(R.string.textoSensorLejos);
         }else if(valor < umbralCerca){
-            return "Cerca";
+            return resources.getString(R.string.textoSensorCerca);
         }else {
-            return "Muy cerca";
+            return resources.getString(R.string.textoSensorMuyCerca);
         }
 
     }
